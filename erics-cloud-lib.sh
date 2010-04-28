09a4ad172c4507faa06979f54b1b539914896152
T. Violet <tviolet@lavabit.com>
2010-04-28 10:53:45 -0400

    when updating bash prompt, just dump entire bashrc file.  Adds test for ls color so that bashrc is compatible with OSX.  Even theough library as a whole is only for debian/ubuntu, I like to have the same bashrc file everywhere

--- erics-cloud-lib.sh
+++ erics-cloud-lib.sh
@@ -80,9 +80,73 @@ better_bash_prompt()
 	local USE_DETAILED=$1
 
 	for bashfile in /root/.bashrc /etc/skel/.bashrc ; do
-		already_added=$(cat $bashfile | egrep "^root.*egrep")
-		if [ -z "$already_added" ] ; then
-			cat <<'EOF' >>$bashfile
+		cat <<'EOF' >$bashfile
+
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines in the history. See bash(1) for more options
+#export HISTCONTROL=ignoredups
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" -a -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+
+
+# enable color support of ls and also add handy aliases
+if [ "$TERM" != "dumb" ]; then
+	
+	color_test=$(ls --color 3>&1 2>&1 >/dev/null)
+	eval "`dircolors -b 2>/dev/null`"
+	
+	if [ -z "$color_test" ] ; then
+		#no errors, alias to ls --color
+		eval "`dircolors -b`"
+    		alias ls='ls --color=auto'
+		alias dir='ls --color=auto --format=vertical'
+		alias vdir='ls --color=auto --format=long'
+
+	else
+		#--color flag doesn't work, use -G (we're probably on OSX)
+		alias ls='ls -G'
+	fi
+fi
+
+# some more ls aliases
+alias ll='ls -al'
+#alias la='ls -A'
+#alias l='ls -CF'
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+#if [ -f /etc/bash_completion ]; then
+#    . /etc/bash_completion
+#fi
+
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+#if [ -f ~/.bash_aliases ]; then
+#    . ~/.bash_aliases
+#fi
+
 
 
 # git is a bitch -- it pipes the output of git diff into less
@@ -187,16 +251,12 @@ short='${debian_chroot:+($debian_chroot)}'$color_user'\u@\h'$color_default':'$co
 
 PS1=$short
 
-EOF
-		fi
-	
+EOF	
 	
 		if [ "$USE_DETAILED" = "1" ] ; then
 			sed -i -e 's/^PS1=\$short[\t ]*$/PS1=$detailed/' $bashfile 
 		fi
 
-
-		sed -i -e "s/^#alias ll='ls -l'/alias ll='ls -al'/" $bashfile # enable ll list long alias <3
 	done
 
 }
