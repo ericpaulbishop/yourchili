b613f0cfc32a68d3268922dd058f59de4323160f
Eric <ericpaulbishop@gmail.com>
2010-05-17 17:08:43 -0400

    when updating cron, make sure old backup cronjob get stripped out properly

--- library/backup_cronjob.sh
+++ library/backup_cronjob.sh
@@ -7,7 +7,7 @@ function setup_backup_cronjob
 	BACKUP_DIR="$2"
 
 	local curr_dir=$(pwd)
-	cat /etc/crontab | grep -v "&&.*do_backup.sh" > /etc/crontab.tmp
+	cat /etc/crontab | grep -v "tmp.back\.sh$" > /etc/crontab.tmp
 	mkdir -p "$BACKUP_DIR"
 	echo "3  23	* * *   root	echo 'source \"$REDCLOUD_INSTALL_DIR/redcloud.sh\" ; backup_mysql root \"$DB_PASSWORD\" \"$BACKUP_DIR\" ; backup_sites \"$BACKUP_DIR\" ; backup_projects \"$BACKUP_DIR\" ; backup_mail_config \"$BACKUP_DIR\" ; backup_hostname \"$BACKUP_DIR\"' > /tmp/back.sh ; chmod 700 /tmp/back.sh ; bash /tmp/back.sh ; rm -rf /tmp/back.sh" >>/etc/crontab.tmp
 	mv /etc/crontab.tmp /etc/crontab
