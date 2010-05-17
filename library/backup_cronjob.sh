405225390fd21a321d874dc4ebc575bbc1032484
Eric <ericpaulbishop@gmail.com>
2010-05-17 16:59:53 -0400

    fix set_backup_cronjob

--- library/backup_cronjob.sh
+++ library/backup_cronjob.sh
@@ -9,7 +9,7 @@ function setup_backup_cronjob
 	local curr_dir=$(pwd)
 	cat /etc/crontab | grep -v "&&.*do_backup.sh" > /etc/crontab.tmp
 	mkdir -p "$BACKUP_DIR"
-	echo "3  23	* * *   root	source \"$REDCLOUD_INSTALL_DIR/redcloud.sh\" ; backup_mysql root \"$DB_PASSWORD\" \"$BACKUP_DIR\" ; backup_sites \"$BACKUP_DIR\" ; backup_projects \"$BACKUP_DIR\" ; backup_mail_config \"$BACKUP_DIR\" ; backup_hostname \"$BACKUP_DIR\"" >>/etc/crontab.tmp
+	echo "3  23	* * *   root	echo 'source \"$REDCLOUD_INSTALL_DIR/redcloud.sh\" ; backup_mysql root \"$DB_PASSWORD\" \"$BACKUP_DIR\" ; backup_sites \"$BACKUP_DIR\" ; backup_projects \"$BACKUP_DIR\" ; backup_mail_config \"$BACKUP_DIR\" ; backup_hostname \"$BACKUP_DIR\"' > /tmp/back.sh ; chmod 700 /tmp/back.sh ; bash /tmp/back.sh ; rm -rf /tmp/back.sh" >>/etc/crontab.tmp
 	mv /etc/crontab.tmp /etc/crontab
 	restart cron
 }
