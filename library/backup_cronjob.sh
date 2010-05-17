519e5b2a38236dba5e5b8644ff92a078c4801c18
Eric <ericpaulbishop@gmail.com>
2010-05-17 15:12:50 -0400

    add necessary parameters to setup_backup_cronjob

--- library/backup_cronjob.sh
+++ library/backup_cronjob.sh
@@ -3,10 +3,13 @@
 
 function setup_backup_cronjob
 {
+	DB_PASSWORD="$1"
+	BACKUP_DIR="$2"
+
 	local curr_dir=$(pwd)
 	cat /etc/crontab | grep -v "&&.*do_backup.sh" > /etc/crontab.tmp
-	mkdir -p /backups
-	echo "3  23	* * *   root	source \"$REDCLOUD_INSTALL_DIR/redcloud.sh\" ; backup_mysql /backups ; backup_sites /backups ; backup_projects /backups ; backup_mail_config /backups ; backup_hostname /backups" >>/etc/crontab.tmp
+	mkdir -p "$BACKUP_DIR"
+	echo "3  23	* * *   root	source \"$REDCLOUD_INSTALL_DIR/redcloud.sh\" ; backup_mysql root \"$DB_PASSWORD\" \"$BACKUP_DIR\" ; backup_sites \"$BACKUP_DIR\" ; backup_projects \"$BACKUP_DIR\" ; backup_mail_config \"$BACKUP_DIR\" ; backup_hostname \"$BACKUP_DIR\"" >>/etc/crontab.tmp
 	mv /etc/crontab.tmp /etc/crontab
 	restart cron
 }
