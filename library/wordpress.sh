04e88c4a387b6996a3f25c72e83ba26c36b62bcd
Eric <ericpaulbishop@gmail.com>
2011-03-31 20:40:07 -0400

    dump known good config file for wp-super-cache instead of modifying sample with sed

--- library/wordpress.sh
+++ library/wordpress.sh
@@ -152,20 +152,129 @@ EOF
 	wget http://downloads.wordpress.org/plugin/wp-super-cache.0.9.9.9.zip
 	unzip wp-super-cache.0.9.9.9.zip
 	rm -rf wp-super-cache.0.9.9.9.zip
-	cp wp-super-cache/wp-cache-config-sample.php ../wp-cache-config.php
-	cd ..
-	sed -i -e 's/^\$cache_enabled.*$/$cache_enabled = true;/g'                                                                wp-cache-config.php
-	sed -i -e 's/^\$super_cache_enabled.*$/$cache_enabled = true;/g'                                                          wp-cache-config.php
-	sed -i -e 's/^.*WPCACHEHOME.*WP_CONTENT_DIR.*$/\tdefine( "WPCACHEHOME", WP_CONTENT_DIR . "/plugins/wp-super-cache" );/g'  wp-cache-config.php
-	sed -i -e 's/^.*\?>.*$/$wp_cache_not_logged_in = 0;\n$wp_cache_not_logged_in = 0;\n$wp_cache_mod_rewrite = 0;\n?>\n/g'    wp-cache-config.php
-	cd ..
+
+
+	# specify wp-super-cache configuration
+	cat << 'EOF' >../wp-cache-config.php
+<?php
+/*
+WP-Cache Config Sample File
+
+See wp-cache.php for author details.
+*/
+
+$wp_cache_mobile_groups = ''; //Added by WP-Cache Manager
+$wp_cache_mobile_prefixes = 'w3c , w3c-, acs-, alav, alca, amoi, audi, avan, benq, bird, blac, blaz, brew, cell, cldc, cmd-, dang, doco, eric, hipt, htc_, inno, ipaq, ipod, jigs, kddi, keji, leno, lg-c, lg-d, lg-g, lge-, lg/u, maui, maxo, midp, mits, mmef, mobi, mot-, moto, mwbp, nec-, newt, noki, palm, pana, pant, phil, play, port, prox, qwap, sage, sams, sany, sch-, sec-, send, seri, sgh-, shar, sie-, siem, smal, smar, sony, sph-, symb, t-mo, teli, tim-, tosh, tsm-, upg1, upsi, vk-v, voda, wap-, wapa, wapi, wapp, wapr, webc, winw, winw, xda , xda-'; //Added by WP-Cache Manager
+$wp_cache_refresh_single_only = '0'; //Added by WP-Cache Manager
+$wp_cache_mod_rewrite = 0; //Added by WP-Cache Manager
+$wp_cache_front_page_checks = 0; //Added by WP-Cache Manager
+$wp_supercache_304 = 0; //Added by WP-Cache Manager
+$wp_cache_slash_check = 0; //Added by WP-Cache Manager
+if ( ! defined('WPCACHEHOME') )
+	define( 'WPCACHEHOME', WP_CONTENT_DIR . "/plugins/wp-super-cache/" ); //Added by WP-Cache Manager
+
+$cache_compression = 0; // Super cache compression
+$cache_enabled = true; //Added by WP-Cache Manager
+$super_cache_enabled = true; //Added by WP-Cache Manager
+$cache_max_time = 3600; //in seconds
+//$use_flock = true; // Set it true or false if you know what to use
+$cache_path = WP_CONTENT_DIR . '/cache/';
+$file_prefix = 'wp-cache-';
+$ossdlcdn = 0;
+
+// We want to be able to identify each blog in a WordPress MU install
+$blogcacheid = '';
+if( defined( 'VHOST' ) ) {
+	$blogcacheid = 'blog'; // main blog
+	if( constant( 'VHOST' ) == 'yes' ) {
+		$blogcacheid = $_SERVER['HTTP_HOST'];
+	} else {
+		$request_uri = preg_replace('/[ <>\'\"\r\n\t\(\)]/', '', str_replace( '..', '', $_SERVER['REQUEST_URI'] ) );
+		if( strpos( $request_uri, '/', 1 ) ) {
+			if( $base == '/' ) {
+				$blogcacheid = substr( $request_uri, 1, strpos( $request_uri, '/', 1 ) - 1 );
+			} else {
+				$blogcacheid = str_replace( $base, '', $request_uri );
+				$blogcacheid = substr( $blogcacheid, 0, strpos( $blogcacheid, '/', 1 ) );
+			}
+			if ( '/' == substr($blogcacheid, -1))
+				$blogcacheid = substr($blogcacheid, 0, -1);
+		}
+		$blogcacheid = str_replace( '/', '', $blogcacheid );
+	}
+}
+
+// Array of files that have 'wp-' but should still be cached 
+$cache_acceptable_files = array( 'wp-comments-popup.php', 'wp-links-opml.php', 'wp-locations.php' );
+
+$cache_rejected_uri = array('wp-.*\\.php', 'index\\.php');
+$cache_rejected_user_agent = array ( 0 => 'bot', 1 => 'ia_archive', 2 => 'slurp', 3 => 'crawl', 4 => 'spider', 5 => 'Yandex' );
+
+$cache_rebuild_files = 1; //Added by WP-Cache Manager
+
+// Disable the file locking system.
+// If you are experiencing problems with clearing or creating cache files
+// uncommenting this may help.
+$wp_cache_mutex_disabled = 1; //Added by WP-Cache Manager
+
+// Just modify it if you have conflicts with semaphores
+$sem_id = 1816471371; //Added by WP-Cache Manager
+
+if ( '/' != substr($cache_path, -1)) {
+	$cache_path .= '/';
+}
+
+$wp_cache_mobile = 0;
+$wp_cache_mobile_whitelist = 'Stand Alone/QNws';
+$wp_cache_mobile_browsers = '2.0 MMP, 240x320, 400X240, AvantGo, BlackBerry, Blazer, Cellphone, Danger, DoCoMo, Elaine/3.0, EudoraWeb, Googlebot-Mobile, hiptop, IEMobile, KYOCERA/WX310K, LG/U990, MIDP-2., MMEF20, MOT-V, NetFront, Newt, Nintendo Wii, Nitro, Nokia, Opera Mini, Palm, PlayStation Portable, portalmmm, Proxinet, ProxiNet, SHARP-TQ-GX10, SHG-i900, Small, SonyEricsson, Symbian OS, SymbianOS, TS21i-10, UP.Browser, UP.Link, webOS, Windows CE, WinWAP, YahooSeeker/M1A1-R2D2, iPhone, iPod, Android, BlackBerry9530, LG-TU915 Obigo, LGE VX, webOS, Nokia5800'; //Added by WP-Cache Manager
+
+// change to relocate the supercache plugins directory
+$wp_cache_plugins_dir = WPCACHEHOME . 'plugins';
+// set to 1 to do garbage collection during normal process shutdown instead of wp-cron
+$wp_cache_shutdown_gc = 0; 
+$wp_super_cache_late_init = 0; //Added by WP-Cache Manager
+
+// uncomment the next line to enable advanced debugging features
+$wp_super_cache_advanced_debug = 0;
+$wp_super_cache_front_page_text = '';
+$wp_super_cache_front_page_clear = 0;
+$wp_super_cache_front_page_check = 0;
+$wp_super_cache_front_page_notification = '0';
+
+$wp_cache_object_cache = 0; //Added by WP-Cache Manager
+$wp_cache_anon_only = 0;
+$wp_supercache_cache_list = 0; //Added by WP-Cache Manager
+$wp_cache_debug_to_file = 0;
+$wp_super_cache_debug = 0;
+$wp_cache_debug_level = 5;
+$wp_cache_debug_ip = '';
+$wp_cache_debug_log = '';
+$wp_cache_debug_email = '';
+$wp_cache_pages[ "search" ] = 0;
+$wp_cache_pages[ "feed" ] = 0;
+$wp_cache_pages[ "category" ] = 0;
+$wp_cache_pages[ "home" ] = 0;
+$wp_cache_pages[ "frontpage" ] = 0;
+$wp_cache_pages[ "tag" ] = 0;
+$wp_cache_pages[ "archives" ] = 0;
+$wp_cache_pages[ "pages" ] = 0;
+$wp_cache_pages[ "single" ] = 0;
+$wp_cache_hide_donation = 0;
+$wp_cache_not_logged_in = 0; //Added by WP-Cache Manager
+$wp_cache_clear_on_post_edit = 0; //Added by WP-Cache Manager
+$wp_cache_hello_world = 0; //Added by WP-Cache Manager
+$wp_cache_mobile_enabled = 1; //Added by WP-Cache Manager
+$wp_cache_cron_check = 0;
+?>	
+EOF
 	
+
 	#activate wp-super-cache plugin
 	echo "UPDATE wp_options SET option_value='/index.php/archives/%post_id%' WHERE option_name='permalink_structure' ;"              | mysql --user="$db" --password="$WP_PASS" $db
 	echo "UPDATE wp_options SET option_value='a:1:{i:0;s:27:\"wp-super-cache/wp-cache.php\";}' WHERE option_name='active_plugins' ;" | mysql --user="$db" --password="$WP_PASS" $db
 	echo "UPDATE wp_options SET autoload='yes' WHERE option_name='active_plugins' ;"                                                 | mysql --user="$db" --password="$WP_PASS" $db
-
-
+	
+	cd "$VPATH"
 	chmod 640 wp-config.php
 	chown -R www-data *
 	chgrp -R www-data *
