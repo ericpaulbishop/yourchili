223b98e844ebf544af3d375f1f4e6756af653400
Eric <ericpaulbishop@gmail.com>
2011-04-03 17:10:40 -0400

    update variable names for chiliproject

--- library/chiliproject.sh
+++ library/chiliproject.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
-############
-# Redmine  #
-############
+#################
+# ChiliProject  #
+#################
 
 
-function create_redmine_project
+function create_chili_project
 {
 	#arguments
-	local DB_PASSWORD=$1                 #root database password, necessary to create database
-	local REDMINE_ID=$2                  #id for redmine installation, will be installed to /srv/projects/redmine/$REDMINE_ID
-	local PROJ_ID=$3                     #id for project being created, this will appear in SCM URLs
-	local IS_PUBLIC=$4                   #is this project publicly visible?  Can anyone grab the code?
-	local SCM=$5                         #SCM to use, currently only "git" and "svn" are supported
-	local PROJ_NAME=$6                   #project name, as it will appear in redmine
-	local REDMINE_ADMIN_USER=$7          #redmine admin user name
-	local REDMINE_ADMIN_PW=$8            #doubles as redmine admin user pw & redmine database pw
-	local REDMINE_ADMIN_FIRST=$9         #first name of redmine admin user
-	local REDMINE_ADMIN_LAST=${10}       #last name of redmine admin user
-	local REDMINE_ADMIN_EMAIL=${11}      #email of redmine admin user
-	local FORCE_SSL_AUTH=${12}           #return unauthorized if someone tries to perform password-protected operation over http and not https, only works when SCM=git
+	local DB_PASSWORD=$1               #root database password, necessary to create database
+	local CHILI_ID=$2                  #id for chili installation, will be installed to /srv/projects/chili/$CHILI_ID
+	local PROJ_ID=$3                   #id for project being created, this will appear in SCM URLs
+	local IS_PUBLIC=$4                 #is this project publicly visible?  Can anyone grab the code?
+	local SCM=$5                       #SCM to use, currently only "git" and "svn" are supported
+	local PROJ_NAME=$6                 #project name, as it will appear in chili
+	local CHILI_ADMIN_USER=$7          #chili admin user name
+	local CHILI_ADMIN_PW=$8            #doubles as chili admin user pw & chili database pw
+	local CHILI_ADMIN_FIRST=$9         #first name of chili admin user
+	local CHILI_ADMIN_LAST=${10}       #last name of chili admin user
+	local CHILI_ADMIN_EMAIL=${11}      #email of chili admin user
+	local FORCE_SSL_AUTH=${12}         #return unauthorized if someone tries to perform password-protected operation over http and not https, only works when SCM=git
 
 	local curdir=$(pwd)
 
-	#create redmine database
-	db="$REDMINE_ID"_rm
+	#create chili database
+	db="$CHILI_ID"_rm
 	mysql_create_database "$DB_PASSWORD" "$db"
-	mysql_create_user     "$DB_PASSWORD" "$db" "$REDMINE_ADMIN_PW"
+	mysql_create_user     "$DB_PASSWORD" "$db" "$CHILI_ADMIN_PW"
 	mysql_grant_user      "$DB_PASSWORD" "$db" "$db"
 
 
-	#In order to clone redmine repo we need git,
+	#In order to clone chili repo we need git,
 	#and gitosis plugin gets installed even if we're using SVN
 	#so just go ahead and install git & gitosis no matter what
 	#does nothing if git/gitosis is already installed
 	git_install
 	gitosis_install
 
-	#redmine
-	mkdir -p /srv/projects/redmine
-	cd /srv/projects/redmine
+	#get chiliproject code
+	mkdir -p /srv/projects/chili
+	cd /srv/projects/chili
 	git clone https://github.com/edavis10/chiliproject.git
-	mv redmine "$REDMINE_ID"
-	cd "$REDMINE_ID"
+	mv chiliproject "$CHILI_ID"
+	cd "$CHILI_ID"
 	git checkout "v1.2.0"
 	rm -rf .git
 
@@ -52,7 +52,7 @@ production:
   database: $db
   host: localhost
   username: $db
-  password: $REDMINE_ADMIN_PW
+  password: $CHILI_ADMIN_PW
 EOF
 
 	if [ -e config/initializers/session_store.rb ] ; then
@@ -61,7 +61,7 @@ EOF
 		rake generate_session_store
 	fi
 	RAILS_ENV=production rake db:migrate
-	echo "en" | RAILS_ENV=production rake redmine:load_default_data
+	echo "en" | RAILS_ENV=production rake chiliproject:load_default_data
 	mkdir tmp public/plugin_assets
 	sudo chmod -R 755 files log tmp public/plugin_assets
 
@@ -87,7 +87,7 @@ EOF
 
 	#SCM stuff
 	if [ "$SCM" = "git" ] ; then
-		create_git "$PROJ_ID" "$REDMINE_ID" "$REDMINE_ADMIN_PW" "$FORCE_SSL_AUTH"
+		create_git "$PROJ_ID" "$CHILI_ID" "$CHILI_ADMIN_PW" "$FORCE_SSL_AUTH"
 		if [ "$is_public" = "true" ] ; then
 			touch "/srv/projects/git/repositories/$PROJ_ID.git/git-daemon-export-ok"
 			chmod -R 775 "/srv/projects/git/repositories/$PROJ_ID.git/git-daemon-export-ok"
@@ -95,7 +95,7 @@ EOF
 		fi
 
 	elif [ "$SCM" = "svn" ] || [ "SCM" = "subversion" ] ; then
-		create_svn "$PROJ_ID" "$REDMINE_ID" "$REDMINE_ADMIN_PW" 
+		create_svn "$PROJ_ID" "$CHILI_ID" "$CHILI_ADMIN_PW" 
 	else
 		echo "not implemented yet"
 		return 1
@@ -103,7 +103,7 @@ EOF
 
 
 
-	#initialize redmine project data with create.rb script
+	#initialize chili project data with create.rb script
 	cat << EOF >create.rb
 # Adapted From: http://github.com/edavis10/redmine_data_generator/blob/37b8acb63a4302281641090949fb0cb87e8b1039/app/models/data_generator.rb#L36
 
@@ -142,14 +142,14 @@ project.save
 
 @user = User.new( 
 					:language => Setting.default_language,
-					:firstname=>"$REDMINE_ADMIN_FIRST",
-					:lastname=>"$REDMINE_ADMIN_LAST",
-					:mail=>"$REDMINE_ADMIN_EMAIL"
+					:firstname=>"$CHILI_ADMIN_FIRST",
+					:lastname=>"$CHILI_ADMIN_LAST",
+					:mail=>"$CHILI_ADMIN_EMAIL"
 					)
 @user.admin = true
-@user.login = "$REDMINE_ADMIN_USER"
-@user.password = "$REDMINE_ADMIN_PW"
-@user.password_confirmation = "$REDMINE_ADMIN_PW"
+@user.login = "$CHILI_ADMIN_USER"
+@user.password = "$CHILI_ADMIN_PW"
+@user.password_confirmation = "$CHILI_ADMIN_PW"
 @user.save
 
 @membership = Member.new(
@@ -188,12 +188,12 @@ EOF
 	cd redmine_gitosis
 	rm -rf .git
 	sed -i -e  "s/'gitosisUrl.*\$/'gitosisUrl' => 'git@localhost:gitosis-admin.git',/"                                         "init.rb"
-	sed -i -e  "s/'gitosisIdentityFile.*\$/'gitosisIdentityFile' => '\/srv\/projects\/redmine\/$REDMINE_ID\/.ssh\/id_rsa',/"   "init.rb"
+	sed -i -e  "s/'gitosisIdentityFile.*\$/'gitosisIdentityFile' => '\/srv\/projects\/redmine\/$CHILI_ID\/.ssh\/id_rsa',/"   "init.rb"
 	sed -i -e  "s/'basePath.*\$/'basePath' => '\/srv\/projects\/git\/repositories\/',/"                                        "init.rb"
-	cp -r ~/.ssh "/srv/projects/redmine/$REDMINE_ID/"
-	chown -R www-data:www-data "/srv/projects/redmine/$REDMINE_ID/"
-	chmod 600 "/srv/projects/redmine/$REDMINE_ID/.ssh/id_rsa"
-	cd "/srv/projects/redmine/$REDMINE_ID/"
+	cp -r ~/.ssh "/srv/projects/chili/$CHILI_ID/"
+	chown -R www-data:www-data "/srv/projects/chili/$CHILI_ID/"
+	chmod 600 "/srv/projects/chili/$CHILI_ID/.ssh/id_rsa"
+	cd "/srv/projects/chili/$CHILI_ID/"
 	rake db:migrate_plugins RAILS_ENV=production
 
 
@@ -202,7 +202,7 @@ EOF
 	git clone https://github.com/ericpaulbishop/redmine_single_project.git
 	cd redmine_single_project
 	rm -rf .git
-	cd "/srv/projects/redmine/$REDMINE_ID/"
+	cd "/srv/projects/chili/$CHILI_ID/"
 
 	#action_mailer_optional_tls plugin
 	script/plugin install git://github.com/collectiveidea/action_mailer_optional_tls.git
@@ -217,7 +217,7 @@ EOF
 
 
 	chown    www-data:www-data /srv/projects
-	chown -R www-data:www-data /srv/projects/redmine
+	chown -R www-data:www-data /srv/projects/chili
 
 	/etc/init.d/nginx restart
 	
@@ -226,27 +226,27 @@ EOF
 }
 
 
-function add_redmine_project
+function add_chili_project
 {
 	#arguments
-	local REDMINE_ID=$1                  #id for redmine installation, will be installed to /srv/projects/redmine/$REDMINE_ID
+	local CHILI_ID=$1                  #id for redmine installation, will be installed to /srv/projects/redmine/$CHILI_ID
 	local PROJ_ID=$2                     #id for project being created, this will appear in SCM URLs
 	local IS_PUBLIC=$3                   #is this project publicly visible?  Can anyone grab the code?
 	local SCM=$4                         #SCM to use, currently only "git" and "svn" are supported
 	local PROJ_NAME=$5                   #project name, as it will appear in redmine
-	local REDMINE_ADMIN_USER=$6          #redmine admin user name, must already exist
-	local REDMINE_ADMIN_PW=$7            #doubles as redmine admin user pw & redmine database pw
+	local CHILI_ADMIN_USER=$6          #redmine admin user name, must already exist
+	local CHILI_ADMIN_PW=$7            #doubles as redmine admin user pw & redmine database pw
 	local FORCE_SSL_AUTH=$8              #return unauthorized if someone tries to perform password-protected operation over http and not https, only works when SCM=git
 	
 	
 	local curdir=$(pwd)
 
 
-	#cd to redmine directory, crap out if it doesn't exist
-	if [ ! -d "/srv/projects/redmine/$REDMINE_ID" ] ; then
+	#cd to chili directory, crap out if it doesn't exist
+	if [ ! -d "/srv/projects/chili/$CHILI_ID" ] ; then
 		return 1 
 	fi
-	cd "/srv/projects/redmine/$REDMINE_ID"
+	cd "/srv/projects/chili/$CHILI_ID"
 
 
 	# save whether we have a public project here 
@@ -268,7 +268,7 @@ function add_redmine_project
 
 	#SCM stuff
 	if [ "$SCM" = "git" ] ; then
-		create_git "$PROJ_ID" "$REDMINE_ID" "$REDMINE_ADMIN_PW" "$FORCE_SSL_AUTH"
+		create_git "$PROJ_ID" "$CHILI_ID" "$CHILI_ADMIN_PW" "$FORCE_SSL_AUTH"
 		if [ "$is_public" = "true" ] ; then
 			touch "/srv/projects/git/repositories/$PROJ_ID.git/git-daemon-export-ok"
 			chmod -R 775 "/srv/projects/git/repositories/$PROJ_ID.git/git-daemon-export-ok"
@@ -276,7 +276,7 @@ function add_redmine_project
 		fi
 
 	elif [ "$SCM" = "svn" ] || [ "SCM" = "subversion" ] ; then
-		create_svn "$PROJ_ID" "$REDMINE_ID" "$REDMINE_ADMIN_PW" 
+		create_svn "$PROJ_ID" "$CHILI_ID" "$CHILI_ADMIN_PW" 
 	else
 		echo "not implemented yet"
 		return 1
@@ -318,7 +318,7 @@ project.enabled_module_names=(["repository", "issue_tracking"])
 project.trackers = Tracker.all
 project.save
 
-@user = User.find(:first, :conditions=>"login = \"$REDMINE_ADMIN_USER\"")
+@user = User.find(:first, :conditions=>"login = \"$CHILI_ADMIN_USER\"")
 
 
 @membership = Member.new(
@@ -344,19 +344,19 @@ EOF
 
 
 
-function enable_redmine_for_vhost
+function enable_chili_for_vhost
 {
 	local VHOST_ID=$1
-	local REDMINE_ID=$2
-	local FORCE_REDMINE_SSL=$3
-	local REDMINE_IS_ROOT=$4
+	local CHILI_ID=$2
+	local FORCE_CHILI_SSL=$3
+	local CHILI_IS_ROOT=$4
 	
 
 	vhost_domain=$(echo $VHOST_ID | sed 's/^www\.//g')
-	gitosis_init="/srv/projects/redmine/$REDMINE_ID/vendor/plugins/redmine_gitosis/init.rb"
+	gitosis_init="/srv/projects/chili/$CHILI_ID/vendor/plugins/redmine_gitosis/init.rb"
 	public=""
-	if [ -e "/srv/projects/redmine/$REDMINE_ID/is_public" ] ; then
-		public=$(grep "true" "/srv/projects/redmine/$REDMINE_ID/is_public")
+	if [ -e "/srv/projects/chili/$CHILI_ID/is_public" ] ; then
+		public=$(grep "true" "/srv/projects/chili/$CHILI_ID/is_public")
 	fi
 	if [ -n "$public" ] ; then
 		sed -i -e  "s/'readOnlyBaseUrl.*\$/'readOnlyBaseUrls' => 'git:\/\/$vhost_domain\/,http:\/\/$vhost_domain\/git\/',/"  "$gitosis_init"
@@ -367,43 +367,43 @@ function enable_redmine_for_vhost
 
 
 
-	#enable redmine in non-ssl vhost, if not forcing use of ssl vhost
+	#enable chili in non-ssl vhost, if not forcing use of ssl vhost
 	local vhost_root=$(cat "/etc/nginx/sites-available/$VHOST_ID" | grep -P "^[\t ]*root"  | awk ' { print $2 } ' | sed 's/;.*$//g')
 	vhost_config="/etc/nginx/sites-available/$VHOST_ID"
 	
-	if [ "$FORCE_REDMINE_SSL" != "1" ] && [ "$FORCE_REDMINE_SSL" != "true" ] ; then
-		ln -s "/srv/projects/redmine/$REDMINE_ID/public"  "$vhost_root/$REDMINE_ID"
-		nginx_add_passenger_uri_for_vhost "$vhost_config" "/$REDMINE_ID"
+	if [ "$FORCE_CHILI_SSL" != "1" ] && [ "$FORCE_CHILI_SSL" != "true" ] ; then
+		ln -s "/srv/projects/chili/$CHILI_ID/public"  "$vhost_root/$CHILI_ID"
+		nginx_add_passenger_uri_for_vhost "$vhost_config" "/$CHILI_ID"
 	fi
 
 
-	#enable redmine and git project in ssl vhost
+	#enable chili and git project in ssl vhost
 	ssl_config="/etc/nginx/sites-available/$NGINX_SSL_ID"
 	if [ ! -e "$ssl_config" ] ; then
-		nginx_create_site "$NGINX_SSL_ID" "localhost" "1" "/$REDMINE_ID" "1"
+		nginx_create_site "$NGINX_SSL_ID" "localhost" "1" "/$CHILI_ID" "1"
 	fi
 	local ssl_root=$(cat "/etc/nginx/sites-available/$NGINX_SSL_ID" | grep -P "^[\t ]*root" | awk ' { print $2 } ' | sed 's/;.*$//g')
-	ln -s "/srv/projects/redmine/$REDMINE_ID/public"   "$ssl_root/$REDMINE_ID"
-	nginx_add_passenger_uri_for_vhost "$ssl_config" "/$REDMINE_ID"
+	ln -s "/srv/projects/chili/$CHILI_ID/public"   "$ssl_root/$CHILI_ID"
+	nginx_add_passenger_uri_for_vhost "$ssl_config" "/$CHILI_ID"
 
 	
 
 	# setup nossl_include
-	nossl_include="$NGINX_CONF_PATH/${REDMINE_ID}_${VHOST_ID}_redmine.conf"
+	nossl_include="$NGINX_CONF_PATH/${CHILI_ID}_${VHOST_ID}_chili.conf"
 	rm -rf "$nossl_include"
-	if [ "$FORCE_REDMINE_SSL" = "1" ] || [ "$FORCE_REDMINE_SSL" = "true" ] ; then
+	if [ "$FORCE_CHILI_SSL" = "1" ] || [ "$FORCE_CHILI_SSL" = "true" ] ; then
 		cat << EOF >>"$nossl_include"
-location ~ ^/$REDMINE_ID/.*\$
+location ~ ^/$CHILI_ID/.*\$
 {
 	rewrite ^(.*)\$ https://\$host\$1 permanent;
 }
 EOF
 	fi
-	if [ "$REDMINE_IS_ROOT" = "1" ] || [ "$REDMINE_IS_ROOT" = "true" ]; then
+	if [ "$CHILI_IS_ROOT" = "1" ] || [ "$CHILI_IS_ROOT" = "true" ]; then
 		cat << EOF >>"$nossl_include"
 location ~ ^/(index\..*)?$
 {
-	rewrite ^(.*)\$ /${REDMINE_ID} permanent;
+	rewrite ^(.*)\$ /${CHILI_ID} permanent;
 }
 EOF
 	fi
