b9a4e3dc5ec732b7fa743f70b75f647016a57c63
Eric <ericpaulbishop@gmail.com>
2011-04-03 20:43:31 -0400

    switch gitosis plugin backon, use new 2011 branch

--- library/chiliproject.sh
+++ library/chiliproject.sh
@@ -187,18 +187,19 @@ EOF
 
 	
 	#gitosis plugin
-	#cd vendor/plugins
-	#git clone https://github.com/ericpaulbishop/redmine_gitosis.git
-	#cd redmine_gitosis
-	#rm -rf .git
-	#sed -i -e  "s/'gitosisUrl.*\$/'gitosisUrl' => 'git@localhost:gitosis-admin.git',/"                                         "init.rb"
-	#sed -i -e  "s/'gitosisIdentityFile.*\$/'gitosisIdentityFile' => '\/srv\/projects\/redmine\/$CHILI_ID\/.ssh\/id_rsa',/"   "init.rb"
-	#sed -i -e  "s/'basePath.*\$/'basePath' => '\/srv\/projects\/git\/repositories\/',/"                                        "init.rb"
-	#cp -r ~/.ssh "/srv/projects/chili/$CHILI_ID/"
-	#chown -R www-data:www-data "/srv/projects/chili/$CHILI_ID/"
-	#chmod 600 "/srv/projects/chili/$CHILI_ID/.ssh/id_rsa"
-	#cd "/srv/projects/chili/$CHILI_ID/"
-	#rake db:migrate_plugins RAILS_ENV=production
+	cd vendor/plugins
+	git clone https://github.com/ericpaulbishop/redmine_gitosis.git
+	cd redmine_gitosis
+	git checkout 2011
+	rm -rf .git
+	sed -i -e  "s/'gitosisUrl.*\$/'gitosisUrl' => 'git@localhost:gitosis-admin.git',/"                                         "init.rb"
+	sed -i -e  "s/'gitosisIdentityFile.*\$/'gitosisIdentityFile' => '\/srv\/projects\/redmine\/$CHILI_ID\/.ssh\/id_rsa',/"   "init.rb"
+	sed -i -e  "s/'basePath.*\$/'basePath' => '\/srv\/projects\/git\/repositories\/',/"                                        "init.rb"
+	cp -r ~/.ssh "/srv/projects/chili/$CHILI_ID/"
+	chown -R www-data:www-data "/srv/projects/chili/$CHILI_ID/"
+	chmod 600 "/srv/projects/chili/$CHILI_ID/.ssh/id_rsa"
+	cd "/srv/projects/chili/$CHILI_ID/"
+	rake db:migrate_plugins RAILS_ENV=production
 
 
 	#single project plugin
