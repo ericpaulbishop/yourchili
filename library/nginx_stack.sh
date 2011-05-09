b7db51b36ac4fe37d332efe93032b2d97a853251
Eric Bishop <ericpaulbishop@gmail.com>
2011-05-09 11:39:16 -0400

    add settings to optimize unix sockets for php-fpm, as per http://www.saltwaterc.eu/nginx-php-fpm-for-high-loaded-websites.html

--- library/nginx_stack.sh
+++ library/nginx_stack.sh
@@ -27,10 +27,21 @@ function php_fpm_install
  
 	#php5-fpm conf
 	php_fpm_conf_file=`grep -R "^listen.*=.*127" /etc/php5/fpm/* | sed 's/:.*$//g' | uniq | head -n 1`
- 
+
+
 	#sockets > ports. Using the 127.0.0.1:9000 stuff needlessly introduces TCP/IP overhead.
-	sed -i 's/listen = 127.0.0.1:9000/listen = \/var\/run\/php-fpm.sock/' $php_fpm_conf_file
+	sed -i 's/listen = 127.0.0.1:9000/listen = \/var\/run\/php-fpm.sock/'  $php_fpm_conf_file
 	
+	#sockets limited by net.core.somaxconn and listen.backlog to 128 by default, so increase this
+	#see http://www.saltwaterc.eu/nginx-php-fpm-for-high-loaded-websites.html
+	sed -i 's/^.*listen.backlog.*$/listen.backlog = 1024/g'                $php_fpm_conf_file
+	echo "net.core.somaxconn=1024" >/etc/sysctl.d/10-unix-sockets.conf
+	sysctl net.core.somaxconn=1024
+	
+	#set max requests to deal with any possible memory leaks
+	sed -i 's/^.*pm.max_requests.*$/pm.max_requests = 1024/g'              $php_fpm_conf_file
+
+
 	#nice strict permissions
 	sed -i 's/;listen.owner = www-data/listen.owner = '"$PHP_FPM_USER"'/'  $php_fpm_conf_file
 	sed -i 's/;listen.group = www-data/listen.group = '"$PHP_FPM_GROUP"'/' $php_fpm_conf_file
